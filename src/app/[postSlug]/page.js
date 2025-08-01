de7c797d7682a30e9650bdd3558bffcf6093e4de
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-08-01 18:55:36 +0800

    feat: implement code snippets with bright on mdx

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,19 +3,17 @@ import React from 'react';
 import BlogHero from '@/components/BlogHero';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { loadBlogPost } from '@/helpers/file-helpers';
-import { BLOG_TITLE } from '@/constants'
+import { BLOG_TITLE } from '@/constants';
+import CodeSnippet from '@/components/CodeSnippet';
 
 import styles from './postSlug.module.css';
 
 export async function generateMetadata({ params }) {
-  const { frontmatter } = await loadBlogPost(
-    params.postSlug
-  );
-
+  const { frontmatter } = await loadBlogPost(params.postSlug);
 
   return {
     title: `${frontmatter.title} • ${BLOG_TITLE}`,
-    description: frontmatter.abstract
+    description: frontmatter.abstract,
   };
 }
 
@@ -31,7 +29,12 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote
+          source={content}
+          components={{
+            pre: CodeSnippet,
+          }}
+        />
       </div>
     </article>
   );
