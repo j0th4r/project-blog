05df3cb569f50d8f0777e59d9844cfcd4f6e2078
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-08-02 15:38:13 +0800

    chore: refactor mdx components & respect user motion preferences

---
+++ src/helpers/mdx-components.js
@@ -0,0 +1,9 @@
+import CodeSnippet from '@/components/CodeSnippet';
+import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
+
+const COMPONENTS_MAP = {
+  pre: CodeSnippet,
+  DivisionGroupsDemo,
+};
+
+export default COMPONENTS_MAP;
