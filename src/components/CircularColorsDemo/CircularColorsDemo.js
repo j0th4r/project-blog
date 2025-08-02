e65cfa97c716103da18c218f926700285bfd9723
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-08-02 17:12:26 +0800

    feat: implement circular colors widget

--- src/components/CircularColorsDemo/CircularColorsDemo.js
+++ src/components/CircularColorsDemo/CircularColorsDemo.js
@@ -1,15 +1,15 @@
+'use client';
 import React from 'react';
 import clsx from 'clsx';
-import {
-  Play,
-  Pause,
-  RotateCcw,
-} from 'react-feather';
+import { Play, Pause, RotateCcw } from 'react-feather';
 
 import Card from '@/components/Card';
 import VisuallyHidden from '@/components/VisuallyHidden';
 
 import styles from './CircularColorsDemo.module.css';
+import { useImmerReducer } from 'use-immer';
+
+import { motion } from 'framer-motion';
 
 const COLORS = [
   { label: 'red', value: 'hsl(348deg 100% 60%)' },
@@ -17,46 +17,80 @@ const COLORS = [
   { label: 'blue', value: 'hsl(235deg 100% 65%)' },
 ];
 
+function reducer(draftState, action) {
+  switch (action.type) {
+    case 'play': {
+      draftState.isRunning = true;
+      return;
+    }
+
+    case 'reset': {
+      draftState.isRunning = false;
+      draftState.count = 0;
+      return;
+    }
+
+    case 'pause': {
+      draftState.isRunning = false;
+      return;
+    }
+
+    case 'run': {
+      draftState.count++;
+      return;
+    }
+  }
+}
+
 function CircularColorsDemo() {
+  const id = React.useId();
+
   // TODO: This value should increase by 1 every second:
-  const timeElapsed = 0;
+  const [timeElapsed, dispatch] = useImmerReducer(reducer, {
+    count: 0,
+    isRunning: false,
+  });
+
+  React.useEffect(() => {
+    if (timeElapsed.isRunning) {
+      const intervalId = window.setInterval(() => {
+        dispatch({ type: 'run' });
+      }, 1000);
+
+      return () => {
+        window.clearInterval(intervalId);
+      };
+    }
+  }, [timeElapsed.isRunning, dispatch]);
 
   // TODO: This value should cycle through the colors in the
   // COLORS array:
-  const selectedColor = COLORS[0];
+  const selectedColor = COLORS[timeElapsed.count % COLORS.length];
 
   return (
     <Card as="section" className={styles.wrapper}>
       <ul className={styles.colorsWrapper}>
         {COLORS.map((color, index) => {
-          const isSelected =
-            color.value === selectedColor.value;
+          const isSelected = color.value === selectedColor.value;
 
           return (
-            <li
-              className={styles.color}
-              key={index}
-            >
+            <li className={styles.color} key={index}>
               {isSelected && (
-                <div
-                  className={
-                    styles.selectedColorOutline
-                  }
+                <motion.div
+                  layoutId={`${id}-selected-color-outline`}
+                  className={styles.selectedColorOutline}
                 />
               )}
               <div
                 className={clsx(
                   styles.colorBox,
-                  isSelected &&
-                    styles.selectedColorBox
+                  isSelected && styles.selectedColorBox
                 )}
                 style={{
                   backgroundColor: color.value,
                 }}
               >
-                <VisuallyHidden>
-                  {color.label}
-                </VisuallyHidden>
+                <VisuallyHidden>{color.label}</VisuallyHidden>
               </div>
             </li>
           );
@@ -66,14 +100,21 @@ function CircularColorsDemo() {
       <div className={styles.timeWrapper}>
         <dl className={styles.timeDisplay}>
           <dt>Time Elapsed</dt>
-          <dd>{timeElapsed}</dd>
+          <dd>{timeElapsed.count}</dd>
         </dl>
         <div className={styles.actions}>
-          <button>
-            <Play />
-            <VisuallyHidden>Play</VisuallyHidden>
-          </button>
-          <button>
+          {timeElapsed.isRunning === false ? (
+            <button onClick={() => dispatch({ type: 'play' })}>
+              <Play />
+              <VisuallyHidden>Play</VisuallyHidden>
+            </button>
+          ) : (
+            <button onClick={() => dispatch({ type: 'pause' })}>
+              <Pause />
+              <VisuallyHidden>Pause</VisuallyHidden>{' '}
+            </button>
+          )}
+          <button onClick={() => dispatch({ type: 'reset' })}>
             <RotateCcw />
             <VisuallyHidden>Reset</VisuallyHidden>
           </button>
